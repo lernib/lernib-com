dda88e38f7f919718698218748bfff0594613e60
frqubit <ckieliszewski@lernib.com>
2024-04-25 14:10:40 -0400

    feat(app): new fonts
    Added Filson Pro and Moderat font files

---
+++ src/utils/font.ts
@@ -0,0 +1,29 @@
+import localFont from "next/font/local";
+
+export const filson_pro_heavy = localFont({
+    src: "../assets/FilsonProHeavy.ttf",
+    weight: "700",
+    style: "normal",
+    display: "swap"
+});
+
+export const moderat_black = localFont({
+    src: "../assets/ModeratBlack.ttf",
+    weight: "900",
+    style: "normal",
+    display: "swap"
+});
+
+export const moderat_bold = localFont({
+    src: "../assets/ModeratBold.ttf",
+    weight: "700",
+    style: "normal",
+    display: "swap"
+});
+
+export const moderat_medium = localFont({
+    src: "../assets/ModeratMedium.ttf",
+    weight: "400",
+    style: "normal",
+    display: "swap"
+});
