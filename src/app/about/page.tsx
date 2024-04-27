3464af98384621209b442a1a2945c2c5d8f2b6cc
frqubit <ckieliszewski@lernib.com>
2024-04-27 16:50:00 -0400

    feat(app): about page
    Added raw about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import PageFooter from "@/components/PageFooter";
+import PageHeader from "@/components/PageHeader";
+
+export default function Home() {
+	return (
+		<>
+			<PageHeader />
+			<PageFooter />
+		</>
+	)
+}
