94cb25b4cdb79617bcff87aeeb1dccab8e243c44
frqubit <ckieliszewski@lernib.com>
2024-04-27 16:44:50 -0400

    refactor(app): editorconfig
    Added editorconfig and modified code to obey

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -5,68 +5,68 @@ import { moderat_black } from "@/utils/font";
 
 
 const H2_CLASS = `
-    ${moderat_black.className}
+	${moderat_black.className}
 
-    text-2xl mb-4
+	text-2xl mb-4
 `;
 
 const FOOTER_DIV_CLASS = `
-    flex flex-col justify-start items-start
-    w-full self-stretch
+	flex flex-col justify-start items-start
+	w-full self-stretch
 `;
 
 export default function PageFooter() {
-    return (
-        <footer className="
-            bg-lernib2 text-lernib1
-            h-fit min-h-40
-            p-12
+	return (
+		<footer className="
+			bg-lernib2 text-lernib1
+			h-fit min-h-40
+			p-12
 
-            gap-x-20 gap-y-20
+			gap-x-20 gap-y-20
 
-            flex flex-col justify-around items-center
+			flex flex-col justify-around items-center
 
-            md:w-full md:flex-row
-            md:pt-12 md:px-40 md:pb-24
-        ">
-            <div className={FOOTER_DIV_CLASS}>
-                <Image
-                    src={logo_white}
-                    alt="logo"
-                    height={REM * 2}
-                    width={REM * 2 * (logo_white.width / logo_white.height)}
-                />
-                <p className="mt-4">
-                    Connect with excellent tutors on our
-                    platform. Learn the way you want, wherever
-                    and however you like.
-                </p>
-            </div>
-            <div className={`
-                ${FOOTER_DIV_CLASS}
+			md:w-full md:flex-row
+			md:pt-12 md:px-40 md:pb-24
+		">
+			<div className={FOOTER_DIV_CLASS}>
+				<Image
+					src={logo_white}
+					alt="logo"
+					height={REM * 2}
+					width={REM * 2 * (logo_white.width / logo_white.height)}
+				/>
+				<p className="mt-4">
+					Connect with excellent tutors on our
+					platform. Learn the way you want, wherever
+					and however you like.
+				</p>
+			</div>
+			<div className={`
+				${FOOTER_DIV_CLASS}
 
-                gap-y-4
-            `}>
-                <h2 className={H2_CLASS}>
-                    Browse
-                </h2>
-                <a href="/">Home</a>
-                <a href="/about">About Us</a>
-            </div>
-            <div className={`
-                ${FOOTER_DIV_CLASS}
+				gap-y-4
+			`}>
+				<h2 className={H2_CLASS}>
+					Browse
+				</h2>
+				<a href="/">Home</a>
+				<a href="/about">About Us</a>
+			</div>
+			<div className={`
+				${FOOTER_DIV_CLASS}
 
-                gap-y-4
-            `}>
-                <h2 className={H2_CLASS}>Services</h2>
-                <a href="/pricing">Pricing</a>
-                <a href="/blog">Blog</a>
-            </div>
-            <div className={`
-                ${FOOTER_DIV_CLASS}
+				gap-y-4
+			`}>
+				<h2 className={H2_CLASS}>Services</h2>
+				<a href="/pricing">Pricing</a>
+				<a href="/blog">Blog</a>
+			</div>
+			<div className={`
+				${FOOTER_DIV_CLASS}
 
-                gap-y-4
-            `} />
-        </footer>
-    )
+				gap-y-4
+			`} />
+		</footer>
+	)
 }
--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,29 +1,29 @@
 import localFont from "next/font/local";
 
 export const filson_pro_heavy = localFont({
-    src: "../assets/fonts/FilsonProHeavy.ttf",
-    weight: "700",
-    style: "normal",
-    display: "swap"
+  src: "../assets/fonts/FilsonProHeavy.ttf",
+  weight: "700",
+  style: "normal",
+  display: "swap"
 });
 
 export const moderat_black = localFont({
-    src: "../assets/fonts/ModeratBlack.ttf",
-    weight: "900",
-    style: "normal",
-    display: "swap"
+  src: "../assets/fonts/ModeratBlack.ttf",
+  weight: "900",
+  style: "normal",
+  display: "swap"
 });
 
 export const moderat_bold = localFont({
-    src: "../assets/fonts/ModeratBold.ttf",
-    weight: "700",
-    style: "normal",
-    display: "swap"
+  src: "../assets/fonts/ModeratBold.ttf",
+  weight: "700",
+  style: "normal",
+  display: "swap"
 });
 
 export const moderat_medium = localFont({
-    src: "../assets/fonts/ModeratMedium.ttf",
-    weight: "400",
-    style: "normal",
-    display: "swap"
+  src: "../assets/fonts/ModeratMedium.ttf",
+  weight: "400",
+  style: "normal",
+  display: "swap"
 });
