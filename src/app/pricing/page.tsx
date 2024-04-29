21f18a78dd131b1bd6c4cb6f1a26dd8dd8a29d35
frqubit <ckieliszewski@lernib.com>
2024-04-29 13:32:47 -0400

    feat(app): pricing contents
    Added contents to pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,10 +1,90 @@
 import PageHeader from "@/components/PageHeader";
 import PageFooter from "@/components/PageFooter";
+import PRICES from "@/data/prices";
+import {
+	filson_pro_heavy,
+	moderat_black,
+	moderat_bold
+} from "@/utils/font";
 
 export default function Home() {
 	return (
 		<>
 			<PageHeader />
+			<div className="
+				bg-lernib1 text-black
+
+				flex-1
+				flex flex-col justify-center items-center
+
+				py-20
+			">
+				<h1 className={`
+					${filson_pro_heavy.className}
+
+					text-center
+					text-4xl
+
+					md:text-7xl
+				`}>
+					Plans &amp; Pricing
+				</h1>
+				<h2 className="
+					text-center
+					mt-4 mx-8
+
+					text-xl md:text-2xl
+				">
+					Pick the best plan for your needs. Upgrade at any time. No
+					<br className="nib-nomobile" />
+					hidden charges, no questions, no hassle.
+				</h2>
+			</div>
+			<main className="
+				bg-lernib1 text-black
+
+				flex flex-col justify-around items-center
+				py-20
+				gap-16
+
+				lg:flex-row lg:p-20
+			">
+				{PRICES.map((price, i) => (
+					<div className="
+						bg-lernib2-700 text-black
+						shadow-lg
+
+						h-80 p-8
+						mx-8
+
+						md:w-full md:m-0
+					">
+						<h2 className={`
+							${moderat_black.className}
+							text-lernib2
+
+							text-4xl
+							mb-8
+						`}>
+							{price.price}
+						</h2>
+
+						<span className={`
+							${moderat_bold.className}
+
+							text-3xl
+						`}>
+							{price.name}
+						</span>
+
+						<p className="
+							mt-4
+						">
+							{price.description}
+						</p>
+					</div>
+				))}
+			</main>
 			<PageFooter />
 		</>
 	)
---
+++ src/data/prices.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    "name": "Group tutoring",
+    "price": "$20",
+    "description": "Group tutoring is in the works. When done, it will\nbe a more affordable but less personalized alternative\nto dedicated tutoring.\n"
+  },
+  {
+    "name": "Private tutoring",
+    "price": "$35",
+    "description": "Private tutoring is ready to go! We offer one-on-one tutoring\nfor various subjects, from computer science to SAT testing.\nThe first session is on us. Contact us to get started!\n"
+  }
+]
