d2f97fe1d585fa884bacd5467ce97c638499d41e
frqubit <ckieliszewski@lernib.com>
2024-05-01 15:03:24 -0400

    feat(app): pardon mess
    Pardon our mess!

---
+++ src/components/InDev.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import styles from "./InDev.module.css";
+import { moderat_black } from "@/utils/font";
+
+
+export default function InDev() {	
+	const [animateClass, setAnimateClass] = useState<string>(styles.preanimation);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setAnimateClass(styles.animation)
+		}, 3000)
+	}, []);
+
+	return (
+		<div className={`
+			${animateClass}
+
+			bg-yellow-400 text-black
+
+			sticky bottom-0
+			w-full py-4
+
+			flex justify-center items-center
+
+
+		`}>
+			<span className={`
+				${moderat_black.className}
+
+				mr-[0.5ch]
+			`}>
+				Pardon our mess!
+			</span>
+
+			<span>
+				We're still setting up our website.
+			</span>
+		</div>
+	)
+}
